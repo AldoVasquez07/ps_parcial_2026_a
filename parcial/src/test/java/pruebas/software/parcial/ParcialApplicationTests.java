ee935f650d199829a43deb800a5e7461c8991981
AldoVasquez07 <avasquezl@ulasalle.edu.pe>
2026-05-08 17:24:23 -0500

    Agregando base del proyecto creado por springinizializr

---
+++ parcial/src/test/java/pruebas/software/parcial/ParcialApplicationTests.java
@@ -0,0 +1,13 @@
+package pruebas.software.parcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
