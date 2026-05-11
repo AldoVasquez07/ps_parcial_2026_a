2f90bcf3fd616af2eedc865104c5191ac43864ee
AldoVasquez07 <avasquezl@ulasalle.edu.pe>
2026-05-10 22:31:16 -0500

    Ajustando pom.xml y borrando archivos no utilizados. Además, se integra un nuevo modelo Usuario para probar las funcionalidades en springboot

---
+++ parcial/src/main/java/pruebas/software/parcial/Usuario.java
@@ -0,0 +1,20 @@
+package pruebas.software.parcial.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String username;
+    private String biografia;
+    private String telefono;
+}
